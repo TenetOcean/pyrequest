36f62383560be2f85f351e38819eb00ce48ee4d1
Li Yang <120624697@qq.com>
2021-08-24 18:27:42 +0800

    新增用例

--- test_interface/test_login.py
+++ test_interface/test_login.py
@@ -15,11 +15,38 @@ class TestLgoinInterface:
         print('end')
 
     def test_user_name_none(self):
-        """用户名为空"""
+        """用户名为None"""
         payload = dict(UserType=1,UserName=None,PassWord='123123',pwd='',UserCode='dfdsg2165498jdofdfd')
         r = requests.post(self.base_url,data=payload)
         result = r.text.split("'")
         assert "系统错误" in result[3]
+
+    def test_user_name_empty(self):
+        """用户名为空"""
+        payload = dict(UserType=1,UserName=" ",PassWord='123123',pwd='',UserCode='dfdsg2165498jdofdfd')
+        r = requests.post(self.base_url,data=payload)
+        result = r.text.split("'")
+        assert "用户名或密码不正确！" in result[3]
+
+    def test_user_password_none(self):
+        """登录密码为None"""
+        payload = dict(UserType=1,UserName="02899999",PassWord=None,pwd='',UserCode='dfdsg2165498jdofdfd')
+        r = requests.post(self.base_url,data=payload)
+        result = r.text.split("'")
+        assert "系统错误" in result[3]
+
+    def test_user_password_empty(self):
+        """登录密码为空"""
+        payload = dict(UserType=1,UserName="02899999",PassWord=" ",pwd='',UserCode='dfdsg2165498jdofdfd')
+        r = requests.post(self.base_url,data=payload)
+        result = r.text.split("'")
+        assert "用户名或密码不正确！" in result[3]
+
+    def test_user_name_and_password_is_right(self):
+        """用户名和密码正确"""
+        payload = dict(UserType=1,UserName="02899999",PassWord="123123",pwd='',UserCode='dfdsg2165498jdofdfd')
+        r = requests.post(self.base_url,data=payload)
+        assert r.status_code == 100
 
 if __name__ == '__main__':
     pytest.main()
